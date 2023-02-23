61a6641d92a408b510f34b001892743fd9d073c8
daw2-perez23 <perezabellanalex@fpllefia.com>
2023-02-23 09:59:57 +0100

    Webserver listo

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const hbs = require('hbs');
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT
+
+
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+//Servir contenido estáticos
+app.use( express.static('public') )
+  
+app.get('/', (req, res) => {
+  res.render( 'home', {
+      nombre: 'Alex Perez',
+      titulo: 'Curso de Node'
+  })
+})
+
+app.get('/generic', (req, res) => {
+    res.render( 'generic', {
+      nombre: 'Alex Perez',
+      titulo: 'Curso de Node'
+  })
+})
+
+app.get('/elements', (req, res) => {
+    res.render( 'elements', {
+      nombre: 'Alex Perez',
+      titulo: 'Curso de Node'
+  })
+})
+
+/* app.get('*', (req, res) => {
+    res.sendFile( __dirname + '/public/404.html' )
+}) */
+  
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
